7c140f7b827e8e62f9728907d2069baf6fd893d2
Andr3wHur5t <Andr3wHur5t@live.com>
2016-06-04 16:49:30 -0700

    Added more secret management tests.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+'use strict';
 const _ = require("lodash");
+const async = require("async")
 const jwt = require('jsonwebtoken');
 const crypto =  require("crypto");
 
@@ -8,12 +10,14 @@ const OTP_ALGORITHMS = {
     md5: "md5",
     rmd160: "rmd160"
 };
+const SUPPORTED_OTP = _.values(OTP_ALGORITHMS);
 
 const JWT_ALGORITHMS = {
     HS256: "HS256",
     HS384: "HS384",
     HS512: "HS512"
 };
+const SUPPORTED_JWT = _.values(JWT_ALGORITHMS);
 
 const DEFAULT_SEED_CONFIG = {
     otpAlgo: OTP_ALGORITHMS.SHA2,
@@ -22,7 +26,7 @@ const DEFAULT_SEED_CONFIG = {
 
 function hmac_helper(key, text, opts) {
     if( !key ) throw new Error("Key must exist to generate hmac!");
-    if( !data ) throw new Error("Data must exist to generate hmac!");
+    if( !text ) throw new Error("Data must exist to generate hmac!");
     opts.algo = opts.algo || OTP_ALGORITHMS.SHA2;
     opts.outputEncoding = opts.outputEncoding || "hex";
     opts.inputEncoding = opts.inputEncoding || "utf8";
@@ -32,18 +36,43 @@ function hmac_helper(key, text, opts) {
 /**
  * Generates a token agent which issues and reads tokens, while managing the life cycle of secrets.
  *
- * @param options The default options for the JWT lib.
+ * @param [options] The default options for the JWT lib.
  * @returns {TokenAgent}
  * @constructor
  */
 var TokenAgent = function TokenAgent (options) {
-    this.issuingSeed = { otpAlgo: OTP_ALGORITHMS.SHA2, jwtAlgo: "HSHA2", secret: "myVoiceIsMyPassword" };
-    // Add on per algo basis
-    this.validSeed = {};
+    this.validSeeds = {};
+    this.jwtOptions = options;
     return this;
 };
 
 TokenAgent.prototype.OTP_ALGORITHMS = OTP_ALGORITHMS;
+TokenAgent.prototype.JWT_ALGORITHMS = JWT_ALGORITHMS;
+
+function prepareSeed(secret, options) {
+    options = _.defaults({secret: secret}, (options || {}), DEFAULT_SEED_CONFIG);
+    if (!_.includes(SUPPORTED_OTP, options.otpAlgo))
+        throw new Error(options.otpAlgo + " isn't a supported OTP algorithm. " + JSON.stringify(SUPPORTED_OTP));
+    if (!_.includes(SUPPORTED_JWT, options.jwtAlgo))
+        throw new Error(options.jwtAlgo + " isn't a supported JWT algorithm. " + JSON.stringify(SUPPORTED_JWT));
+    return options
+}
+
+/**
+ * Sets a secret to be used for generating OTP for signing tokens; will automatically be used for validation.
+ *
+ * @param secret The secret to use for signing
+ * @param [options] The options to use for for OTP generation and JWT signing.
+ */
+TokenAgent.prototype.setIssuingSecret = function setIssuingSeed (secret, options) {
+    this.issuingSeed = prepareSeed(secret, options)
+};
+
+TokenAgent.prototype.addValidationSecret = function setIssuingSeed (secret, options) {
+    var seed = prepareSeed(secret, options);
+    this.validSeeds[seed.jwtAlgo] = this.validSeeds[seed.jwtAlgo] || [];
+    this.validSeeds[seed.jwtAlgo].push(seed);
+};
 
 /**
  * Generates an OTP using the given seed configuration and a timestamp.
@@ -52,7 +81,7 @@ TokenAgent.prototype.OTP_ALGORITHMS = OTP_ALGORITHMS;
  * @param timestamp The timestamp to generate the secret for.
  */
 TokenAgent.prototype.generateOTP = function validOTP (seed, timestamp) {
-    return hmac_helper(seed.secret, (new String(timestamp)), {
+    return hmac_helper(seed.secret, timestamp.toString(), {
         algo: seed.otpAlgo,
         inputEncoding: seed.inputEncoding,
         outputEncoding: seed.outputEncoding
@@ -68,14 +97,13 @@ TokenAgent.prototype.validOTPs = function validOTP (algo, timestamp) {
 
 };
 
-TokenAgent.prototype.JWT_ALGORITHMS = JWT_ALGORITHMS;
-
 /***
  * This generates a new token using the given payload that is signed with a generated OTP made from the issuing secret.
  *
  * @param payload The Object that you wish to put in the token
- * @param options The Options object that is passed to the JWT lib.
- * @param done Optional callback. This function will return sync if no done is provided.
+ * @param [options] The Options object that is passed to the JWT lib.
+ * @param [done] Optional callback. This function will return sync if no done is provided.
+ * @returns {*}
  */
 TokenAgent.prototype.issueToken = function issueToken (payload, options, done) {
     if ( !this.issuingSeed ) throw new Error("An issuing seed must be set before issuing a token!");
@@ -88,11 +116,41 @@ TokenAgent.prototype.issueToken = function issueToken (payload, options, done) {
  * This reads and verifies the signature of the token is in the valid keys list.
  *
  * @param token The signed JWT you wish to read and verify.
- * @param done The optional callback. This function will return sync if no done is provided.
+ * @param [options] To be used by the JWT lib.
+ * @param [done] The optional callback. This function will return sync if no done is provided.
  */
-TokenAgent.prototype.validateToken = function issueToken (token, done) {
-    // TODO: Get the time stamp
-    // Generate the valid secrets for the time stamp
+TokenAgent.prototype.validateToken = function issueToken (token, options, done) {
+    // Get the timestamp from the unverified token
+    var unverifiedPayload = jwt.read(token);
+    var usedAlgorithm = ""; // TODO: Get the algoritm header
+    var validSecrets = this.validOTPs(usedAlgorithm, unverifiedPayload.otp);
+
+    options = _.defaults({algorithms: [usedAlgorithm]}, options, this.jwtOptions);
+
+    var attemptValidation = function (secret, done) {
+        jwt.verify(token, secret, options, function (err, payload) {
+            if(err) payload = undefined;
+            return done(null, payload);
+        })
+    };
+
+    async.each(
+        validSecrets,
+        attemptValidation,
+        function findResult (err, results) {
+            results = _.remove(results, [undefined]);
+            // TODO: Re
+            console.log(results);
+            return done(null, results);
+        }
+    );
 };
+
+/**
+ * This does not provide additional protection, Adding to discourage bad usage.
+ * @returns {string}
+ */
+TokenAgent.prototype.toString = function () { return JSON.stringify(this); };
+TokenAgent.prototype.toJSON = function () { return { "class": "TokenAgent ", information: "Redacted" }; };
 
 module.exports = TokenAgent;
--- tests.js
+++ tests.js
@@ -1,14 +1,45 @@
+var _ = require("lodash");
 var assert = require("assert");
+var timekeeper = require("timekeeper");
 var TokenAgent = require("./index.js");
 
 describe("jwt-otp tokenAgent", function (){
     describe("Secret Management", function () {
+        after(timekeeper.reset);
         it("should set a issuing secret", function () {
-
+            var agent = new TokenAgent();
+            agent.setIssuingSecret("my_voice_is_my_password");
+            assert.equal(agent.issuingSeed.secret, "my_voice_is_my_password");
         });
 
         it("should set validating secrets", function () {
+            var agent = new TokenAgent();
+            agent.addValidationSecret("my_voice_is_my_password");
 
+            timekeeper.travel(new Date("10-26-2016"));
+            agent.addValidationSecret("my_voice_isnt_my_password", {
+                otpAlgo: agent.OTP_ALGORITHMS.md5,
+                expiresAt: new Date().getTime()
+            });
+            assert.deepEqual(
+                agent.validSeeds,
+                {
+                    HS256:
+                    [
+                        {
+                            secret: 'my_voice_is_my_password',
+                            otpAlgo: 'sha256',
+                            jwtAlgo: 'HS256'
+                        },
+                        {
+                            secret: 'my_voice_isnt_my_password',
+                            otpAlgo: 'md5',
+                            jwtAlgo: 'HS256',
+                            expiresAt: 1477465200000
+                        }
+                    ]
+                }
+            );
         });
 
         it("should expire validating secrets", function () {
@@ -16,9 +47,45 @@ describe("jwt-otp tokenAgent", function (){
         });
     });
 
+    describe("OTP Generation", function () {
+        it("should generate unique keys", function () {
+            var makeOTP = TokenAgent.prototype.generateOTP;
+            var seed = {secret: "my_voice_is_my_password" };
+            assert.deepEqual([
+                makeOTP(seed, 1),
+                makeOTP(seed, 2),
+                makeOTP(seed, 3),
+                makeOTP(seed, 4),
+                makeOTP(seed, 5)
+            ], [ '74014a43ed8f78f64b876e1bd9c05343933a74ed7305942c368d2c35064ba3ee',
+                '7949f74c18ed157cd12d927ac4dfc66ff27a697ff7ae670207e40f3c2c383241',
+                'af466286c57c36e835f316e66e6e4f73d772c4385602633017c5db9880af6e8c',
+                'f924653154a1962b44d8dcf0ea86d873277c8de26c26141b97928d3f899dd8f2',
+                'd1e06e3b2d09969ee9c08ed7d4826ada2fbdda03ec6ef009cfd47fb1e955df77' ]);
+
+            _.times(5, function (i) {
+                assert.equal(makeOTP(seed, i), makeOTP(seed, i))
+            })
+
+        });
+
+        it("should generate unique keys for diffrent password", function () {
+            var makeOTP = TokenAgent.prototype.generateOTP;
+            var seed = {secret: "my_voice_is_my_password" };
+            var seed2 = {secret: "my_voice_is_not_my_password"};
+            _.times(5, function (i) {
+                assert.notEqual(makeOTP(seed, i), makeOTP(seed2, i))
+            })
+        });
+    });
+
     describe("Token Issuance", function () {
         it("should issue deterministic tokens", function () {
-
+            var agent = new TokenAgent();
+            agent.setIssuingSecret("my_voice_is_my_password");
+            timekeeper.travel(new Date("10-26-2016"));
+            assert.equal(agent.issueToken({iLike: "cake"}),
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpTGlrZSI6ImNha2UiLCJvdHAiOjE0Nzc0NjUyMDAwMDAsImlhdCI6MTQ3NzQ2NTIwMH0.suSiapybRNaV7jUZhRbz3a76_6ODQrAi4kKvTlQzqCY")
         });
 
         it("should issue a validatable token", function () {
